07642f5b3617637992c4d91b41313677bcbd0833
shiva-s30 <shivasrisailam1999@gmail.com>
2023-11-25 19:33:37 +0530

    schema.py updation

---
+++ app/schema.py
@@ -0,0 +1,20 @@
+from typing import Union
+from pydantic import BaseModel as PydBaseModel
+
+class Item(PydBaseModel):
+    id: int
+    owner_id: int
+    title: str
+    description: Union[str, None] = None
+
+    class Config:
+        orm_mode = True
+
+
+class User(PydBaseModel):
+    id: int
+    is_active: bool
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
