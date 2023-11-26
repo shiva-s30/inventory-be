f260769d53185ed5f488036c4feda4e72c709cb1
shiva-s30 <shivasrisailam1999@gmail.com>
2023-11-26 20:16:57 +0530

    updates

--- app/crud.py
+++ app/crud.py
@@ -19,17 +19,17 @@ def create_user(db: Session, user: schema.UserCreate):
     db_user = models.User(email=user.email, password=user.password)
     db.add(db_user)
     db.commit()
-    db.refresh()
+    db.refresh(db_user)
     return db_user
 
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).first()
+    return db.query(models.Item).offset(skip).limit(limit).all()
 
 
 def create_user_item(db: Session, item: schema.ItemCreate, user_id: int):
     db_item = models.Item(**item.model_dump(), owner_id=user_id)
     db.add(db_item)
     db.commit()
-    db.refresh()
+    db.refresh(db_item)
     return db_item
